fb942f38ec7ec8ae9c950c9fbded7ea843dab42c
Amrou-Alwane <amroualwane1@gmail.com>
2023-05-19 01:54:55 +0200

    conflit resolved

--- zaraapp/client/componenets/header.tsx
+++ zaraapp/client/componenets/header.tsx
@@ -1,52 +1,49 @@
-"use client"
-
-import React, { FC , useState , useEffect , useRef} from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import '@/styles/ff.module.css';
-import Image from 'next/image'
+import Image from 'next/image';
 import axios from 'axios';
 import Link from 'next/link';
-import Woman from '../app/Woman/page.tsx'
-import Men from '../app/Men/page.tsx'
+import Woman from '../app/Woman/page.tsx';
+import Men from '../app/Men/page.tsx';
+
 const Header: FC = () => {
-
   const [isMenuOpen, setMenuOpen] = useState(false);
-  const [data , setData] = useState([])
-  const [display,setDisplay]=useState(false)
-  const handleDisplay=()=>{
-    setDisplay(!display)
-  }
-import Woman from '../app/Woman/page.tsx'
-const Header: FC = () => {
+  const [data, setData] = useState([]);
+  const [display, setDisplay] = useState(false);
 
-  const [isMenuOpen, setMenuOpen] = useState(false);
-  const [data , setData] = useState([]);
-  const [isLoaded , setIsLoaded] = useState(false);
+  const handleDisplay = () => {
+    setDisplay(!display);
+  };
 
-  const fetchData = () => { 
-   axios.get('http://localhost:5000/api/products/')
-     .then(response => {
-    setData(response.data)
-     
-       
-     })
-     .catch(error => {
+  const fetchData = () => {
+    axios
+      .get('http://localhost:5000/api/products/')
+      .then(response => {
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
 
-       console.error(error);
-     });
- };
-
-
-
-useEffect(()=> {
-fetchData()
-},[])
-
-
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <>
-      <header style={{ width: '100%', padding: '12px 24px 20px 24px', position: 'fixed', margin: '0px' }}>
-        <div id="headerContents" style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+      <header
+        style={{
+          width: '100%',
+          padding: '12px 24px 20px 24px',
+          position: 'fixed',
+          margin: '0px',
+        }}
+      >
+        <div
+          id="headerContents"
+          style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}
+        >
           <div id="headerLeft" style={{ display: 'flex' }}>
             <div>
               <input
@@ -54,13 +51,12 @@ fetchData()
                 name="menu"
                 src="https://icon-library.com/images/menu-icon-png-3-lines/menu-icon-png-3-lines-14.jpg"
                 id="menu"
-                style={{ width: '30px', height: '30px', marginRight: '30px' ,  }}
+                style={{ width: '30px', height: '30px', marginRight: '30px' }}
                 onClick={() => setMenuOpen(!isMenuOpen)}
-
               />
             </div>
-            {isMenuOpen ? null : (
-              <Link  href = "/">
+            {!isMenuOpen && (
+              <Link href="/">
                 <img
                   src="https://upload.wikimedia.org/wikipedia/commons/f/fd/Zara_Logo.svg"
                   alt="Zara_Logo"
@@ -70,25 +66,74 @@ fetchData()
               </Link>
             )}
           </div>
-          <div id="headerRight" style={{ width: '30%', display: 'flex', marginRight: '30px', padding: '10px', justifyContent: 'space-between' }}>
+          <div
+            id="headerRight"
+            style={{
+              width: '30%',
+              display: 'flex',
+              marginRight: '30px',
+              padding: '10px',
+              justifyContent: 'space-between',
+            }}
+          >
             <div id="headerRightLeft">
-              <Link href="/Search" style={{ padding: '0% 60% 0% 0%', fontSize: '13px', textDecoration: 'none', borderBottom: '1px solid black', color: 'rgb(41, 41, 41)', fontWeight: 700, fontFamily: 'Neue-Helvetica, Helvetica, Arial, Sans-Serif', fontStretch: 'ultra-condensed' }}>
+              <Link
+                href="/Search"
+                style={{
+                  padding: '0% 60% 0% 0%',
+                  fontSize: '13px',
+                  textDecoration: 'none',
+                  borderBottom: '1px solid black',
+                  color: 'rgb(41, 41, 41)',
+                  fontWeight: 700,
+                  fontFamily: 'Neue-Helvetica, Helvetica, Arial, Sans-Serif',
+                  fontStretch: 'ultra-condensed',
+                }}
+              >
                 SEARCH
               </Link>
             </div>
             <div id="headerRightRight">
-           
-  <Link href="/Login" style={{ textDecoration: 'none', padding: '0px 10px 0px 10px', fontSize: '13px', color: 'rgb(41, 41, 41)', fontWeight: 100, fontFamily: 'Neue-Helvetica, Helvetica, Arial, Sans-Serif', fontStretch: 'ultra-condensed' }}>
-  <Link href="/Login" style={{ textDecoration: 'none', padding: '0px 10px 0px 10px', fontSize: '13px', color: 'rgb(41, 41, 41)', fontWeight: 100, fontFamily: 'Neue-Helvetica, Helvetica, Arial, Sans-Serif', fontStretch: 'ultra-condensed' }}>
-    LOG IN
- 
-</Link>
-
-
-              <a href="" style={{ textDecoration: 'none', padding: '0px 10px 0px 10px', fontSize: '13px', color: 'rgb(41, 41, 41)', fontWeight: 100, fontFamily: 'Neue-Helvetica, Helvetica, Arial, Sans-Serif', fontStretch: 'ultra-condensed' }}>
+              <Link
+                href="/Login"
+                style={{
+                  textDecoration: 'none',
+                  padding: '0px 10px 0px 10px',
+                  fontSize: '13px',
+                  color: 'rgb(41, 41, 41)',
+                  fontWeight: 100,
+                  fontFamily: 'Neue-Helvetica, Helvetica, Arial, Sans-Serif',
+                  fontStretch: 'ultra-condensed',
+                }}
+              >
+                LOG IN
+              </Link>
+              <a
+                href=""
+                style={{
+                  textDecoration: 'none',
+                  padding: '0px 10px 0px 10px',
+                  fontSize: '13px',
+                  color: 'rgb(41, 41, 41)',
+                  fontWeight: 100,
+                  fontFamily: 'Neue-Helvetica, Helvetica, Arial, Sans-Serif',
+                  fontStretch: 'ultra-condensed',
+                }}
+              >
                 HELP
               </a>
-              <Link href="./Cart" style={{ textDecoration: 'none', padding: '0px 10px 0px 10px', fontSize: '13px', color: 'rgb(41, 41, 41)', fontWeight: 100, fontFamily: 'Neue-Helvetica, Helvetica, Arial, Sans-Serif', fontStretch: 'ultra-condensed' }}>
+              <Link
+                href="./Cart"
+                style={{
+                  textDecoration: 'none',
+                  padding: '0px 10px 0px 10px',
+                  fontSize: '13px',
+                  color: 'rgb(41, 41, 41)',
+                  fontWeight: 100,
+                  fontFamily: 'Neue-Helvetica, Helvetica, Arial, Sans-Serif',
+                  fontStretch: 'ultra-condensed',
+                }}
+              >
                 CART
               </Link>
             </div>
@@ -96,67 +141,113 @@ fetchData()
         </div>
       </header>
       {isMenuOpen && (
-       <div id="menuModal" className="menuOpacity animatedMenuShow">
-       <div className="menuContent">
-         <header id="headerRight" style={{ width: '1000%', display: 'flex', marginRight: '30px', padding: '10px', justifyContent: 'space-between' }}>
-           <div id="menuTop">
-          
-           </div>
-         </header>
-         <div id="menuOptions" style={{ position: 'fixed', overflow: 'hidden', height: '1000px', width: '500px', backgroundColor: 'white'  , marginTop : "-20px"}}>
-           <div id="menuMainOptions" style={{ display: 'flex'  }}>
-             <ul className="menuOptionsList">
-             <ul className="close" onClick={() => setMenuOpen(false)}>
-               ×
-             </ul>
+        <div id="menuModal" className="menuOpacity animatedMenuShow">
+          <div className="menuContent">
+            <header
+              id="headerRight"
+              style={{
+                width: '1000%',
+                display: 'flex',
+                marginRight: '30px',
+                padding: '10px',
+                justifyContent: 'space-between',
+              }}
+            >
+              <div id="menuTop"></div>
+            </header>
+            <div
+              id="menuOptions"
+              style={{
+                position: 'fixed',
+                overflow: 'hidden',
+                height: '1000px',
+                width: '500px',
+                backgroundColor: 'white',
+                marginTop: '-20px',
+              }}
+            >
+              <div id="menuMainOptions" style={{ display: 'flex' }}>
+                <ul className="menuOptionsList">
+                  <ul className="close" onClick={() => setMenuOpen(false)}>
+                    ×
+                  </ul>
 
-            
+                  <div
+                    style={{
+                      marginTop: '90px',
+                      display: 'flex',
+                      flexWrap: 'wrap',
+                      marginBottom: '25px',
+                    }}
+                  >
+                    <p
+                      className="menuOption"
+                      style={{ marginRight: '10px' }}
+                      onClick={() => setDisplay(!display)}
+                    >
+                      WOMAN
+                    </p>
+                    <p className="menuOption" style={{ marginRight: '10px' }} onClick={handleDisplay}>
+                      MEN
+                    </p>
+                    <p className="menuOption" style={{ marginRight: '10px' }}>
+                      KIDS
+                    </p>
+                    <p className="menuOption" style={{ marginRight: '10px' }}>
+                      BEAUTY
+                    </p>
+                  </div>
 
-  <div style={{ marginTop: "90px", display: 'flex', flexWrap: 'wrap' , marginBottom : '25px' }}>
-    <p className="menuOption" style={{ marginRight: '10px' }} onClick={()=>setIsLoaded(!isLoaded)} >WOMAN</p>
-    <p className="menuOption" style={{ marginRight: '10px' }} onClick={handleDisplay}>MEN</p>
-    <p className="menuOption" style={{ marginRight: '10px' }}>KIDS</p>
-    <p className="menuOption" style={{ marginRight: '10px' }}>BEAUTY</p>
-  </div>
+                  <li className="menuOption">NEW</li>
+                  {display ? (
+                    <div className="menuOption">
+                      <Men />
+                    </div>
+                  ) : (
+                    <></>
+                  )}
 
-
-               <li className="menuOption">NEW</li> 
-           {display ? <li className="menuOption"><Men/></li>:<></>}
-                  
-    <p className="menuOption" style={{ marginRight: '10px' }} onClick={()=>setIsLoaded(!isLoaded)} >WOMAN</p>
-    <p className="menuOption" style={{ marginRight: '10px' }}>MAN</p>
-    <p className="menuOption" style={{ marginRight: '10px' }}>KIDS</p>
-    <p className="menuOption" style={{ marginRight: '10px' }}>BEAUTY</p>
-  </div>
-{isLoaded ? 
-               <div className="menuOption"><Woman/></div>
-  : <div></div>
-}
-           </ul>
-           </div>
-           <div id="menuExtraOptions">
-             <ul>
-               <li>JOIN LIFE</li>
-               <li>+ INFO</li>
-             </ul>
-           </div>
-         </div>
-       </div>
-     </div>
-
-      ) }
-
-
-
-
-
-
-
-</>
-);
+                  <p
+                    className="menuOption"
+                    style={{ marginRight: '10px' }}
+                    onClick={() => setDisplay(!display)}
+                  >
+                    WOMAN
+                  </p>
+                  <p className="menuOption" style={{ marginRight: '10px' }}>
+                    MAN
+                  </p>
+                  <p className="menuOption" style={{ marginRight: '10px' }}>
+                    KIDS
+                  </p>
+                  <p className="menuOption" style={{ marginRight: '10px' }}>
+                    BEAUTY
+                  </p>
+                </ul>
+              </div>
+              {isLoaded ? (
+                <div className="menuOption">
+                  <Woman />
+                </div>
+              ) : (
+                <div></div>
+              )}
+            </div>
+            <div id="menuExtraOptions">
+              <ul>
+                <li>JOIN LIFE</li>
+                <li>+ INFO</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Header;
+
 
 
 
